0f6597597da31525476bc5316912350c002c3462
Dias-Ondiris <dias.ondiris2@gmail.com>
2023-12-10 23:30:46 +0600

    Добавьте файлы проекта.

---
+++ Modul15.Prak/MyClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul15.Prak
+{
+    public class MyClass
+    {
+        public int PublicProperty { get; set; }
+        private string PrivateProperty { get; set; }
+        public MyClass() { }
+        public MyClass(int value)
+        {
+            PublicProperty = value;
+        }
+        private MyClass(string message)
+        {
+            PrivateProperty = message;
+        }
+        public void PublicMethod()
+        {
+            Console.WriteLine("Public Method Called");
+        }
+        private void PrivateMethod()
+        {
+            Console.WriteLine("Private Method Called: " + PrivateProperty);
+        }
+    }
+
+}
---
+++ Modul15.Prak/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Modul15.Prak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Type myClassType = typeof(MyClass);
+
+            //Задание1 Исследование типа
+            Console.WriteLine("Class Name: " + myClassType.Name);
+
+            Console.WriteLine("\nConstructors:");
+            foreach (var constructor in myClassType.GetConstructors(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic))
+            {
+                Console.WriteLine(constructor);
+            }
+
+            Console.WriteLine("\nProperties:");
+            foreach (var prop in myClassType.GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic))
+            {
+                Console.WriteLine($"{prop.PropertyType} {prop.Name}");
+            }
+
+            Console.WriteLine("\nMethods:");
+            foreach (var method in myClassType.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic))
+            {
+                Console.WriteLine($"{method.ReturnType} {method.Name}");
+            }
+
+            //Задание2 Создание экземпляра
+            var myClassInstance = (MyClass)Activator.CreateInstance(myClassType);
+
+            //Задание3 Манипулирование объектом
+            PropertyInfo publicProperty = myClassType.GetProperty("PublicProperty");
+            publicProperty.SetValue(myClassInstance, 10);
+
+            MethodInfo publicMethod = myClassType.GetMethod("PublicMethod");
+            publicMethod.Invoke(myClassInstance, null);
+
+            //Задание4 Расширенное исследование
+            ConstructorInfo privateConstructor = myClassType.GetConstructor(BindingFlags.Instance | BindingFlags.NonPublic, null, new[] { typeof(string) }, null);
+            var myClassPrivateInstance = (MyClass)privateConstructor.Invoke(new object[] { "Hello" });
+
+            MethodInfo privateMethod = myClassType.GetMethod("PrivateMethod", BindingFlags.Instance | BindingFlags.NonPublic);
+            privateMethod.Invoke(myClassPrivateInstance, null);
+            Console.ReadKey();
+        }
+    }
+}
